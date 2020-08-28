9a8dfc241f8b7fff854dc4a24e3aca8728e488ac
Yushan Gao <ysgao@thoughtworks.com>
2020-08-28 22:53:46 +0800

    refactor: change class name

--- back-end/src/main/java/com/happy3w/lifecompass/LifeCompassApplication.java
+++ back-end/src/main/java/com/happy3w/lifecompass/LifeCompassApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LifecompassApplication {
+public class LifeCompassApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LifecompassApplication.class, args);
+		SpringApplication.run(LifeCompassApplication.class, args);
 	}
 
 }
