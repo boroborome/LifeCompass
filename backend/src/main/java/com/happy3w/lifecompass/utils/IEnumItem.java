5d80cdaedaa5d7a60bcb1370c1174c4c6c9e0f93
Yushan Gao <ysgao@thoughtworks.com>
2020-09-05 14:39:01 +0800

    feat: make Enum json beautyful

--- backend/src/main/java/com/happy3w/lifecompass/utils/IEnumItem.java
+++ backend/src/main/java/com/happy3w/lifecompass/utils/IEnumItem.java
@@ -1,5 +1,8 @@
 package com.happy3w.lifecompass.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public interface IEnumItem {
     String getCode();
     String getName();
