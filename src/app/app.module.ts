c623ac53dc404c0dec029eae36a1b6657f9b7432
Yushan Gao <ysgao@thoughtworks.com>
2020-09-05 16:39:33 +0800

    feat: add project-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectViewComponent } from './views/project-view/project-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectViewComponent
   ],
   imports: [
     BrowserModule,
