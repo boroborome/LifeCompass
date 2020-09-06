847bd6a49fb2833b7f2c49feea486534ef1c5c3e
Yushan Gao <ysgao@thoughtworks.com>
2020-09-06 20:59:45 +0800

    feat: add service-path configuration for backend

--- backend/src/main/java/com/happy3w/lifecompass/controller/MasterController.java
+++ backend/src/main/java/com/happy3w/lifecompass/controller/MasterController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/master")
+@RequestMapping("${life-compass.service-path}/master")
 public class MasterController {
     @ResponseBody
     @RequestMapping(value = "task-status", method = RequestMethod.GET)
