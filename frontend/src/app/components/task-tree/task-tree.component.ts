9c57858c213e698473c4e3873956936a2362787a
Yushan Gao <ysgao@thoughtworks.com>
2020-09-08 21:04:22 +0800

    fix: lost task-tree component

---
+++ frontend/src/app/components/task-tree/task-tree.component.ts
@@ -0,0 +1,152 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import {FlatTreeControl} from "@angular/cdk/tree";
+import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
+import {MasterService} from "../../services/master.service";
+
+const LOAD_MORE = 'LOAD_MORE';
+
+/** Nested node */
+export class LoadmoreNode {
+  childrenChange = new BehaviorSubject<LoadmoreNode[]>([]);
+
+  get children(): LoadmoreNode[] {
+    return this.childrenChange.value;
+  }
+
+  constructor(public item: string,
+              public hasChildren = false,
+              public loadMoreParentItem: string | null = null) { }
+}
+
+/** Flat node with expandable and level information */
+export class LoadmoreFlatNode {
+  constructor(public item: string,
+              public level = 1,
+              public expandable = false,
+              public loadMoreParentItem: string | null = null) { }
+}
+
+@Injectable()
+export class LoadmoreDatabase {
+  batchNumber = 5;
+  dataChange = new BehaviorSubject<LoadmoreNode[]>([]);
+  nodeMap = new Map<string, LoadmoreNode>();
+
+  /** The data */
+  rootLevelNodes: string[] = ['Vegetables', 'Fruits'];
+  dataMap = new Map<string, string[]>([
+    ['Fruits', ['Apple', 'Orange', 'Banana']],
+    ['Vegetables', ['Tomato', 'Potato', 'Onion']],
+    ['Apple', ['Fuji', 'Macintosh']],
+    ['Onion', ['Yellow', 'White', 'Purple', 'Green', 'Shallot', 'Sweet', 'Red', 'Leek']],
+  ]);
+
+  // tslint:disable-next-line: typedef
+  initialize() {
+    const data = this.rootLevelNodes.map(name => this._generateNode(name));
+    this.dataChange.next(data);
+  }
+
+  /** Expand a node whose children are not loaded */
+  // tslint:disable-next-line: typedef
+  loadMore(item: string, onlyFirstTime = false) {
+    if (!this.nodeMap.has(item) || !this.dataMap.has(item)) {
+      return;
+    }
+    // tslint:disable-next-line: no-non-null-assertion
+    const parent = this.nodeMap.get(item)!;
+    const children = this.dataMap.get(item)!;
+    if (onlyFirstTime && parent.children!.length > 0) {
+      return;
+    }
+    const newChildrenNumber = parent.children!.length + this.batchNumber;
+    const nodes = children.slice(0, newChildrenNumber)
+      .map(name => this._generateNode(name));
+    if (newChildrenNumber < children.length) {
+      // Need a new load more node
+      nodes.push(new LoadmoreNode(LOAD_MORE, false, item));
+    }
+
+    parent.childrenChange.next(nodes);
+    this.dataChange.next(this.dataChange.value);
+  }
+
+  private _generateNode(item: string): LoadmoreNode {
+    if (this.nodeMap.has(item)) {
+      return this.nodeMap.get(item)!;
+    }
+    const result = new LoadmoreNode(item, this.dataMap.has(item));
+    this.nodeMap.set(item, result);
+    return result;
+  }
+}
+
+@Component({
+  selector: 'app-task-tree',
+  templateUrl: './task-tree.component.html',
+  styleUrls: ['./task-tree.component.scss'],
+  providers: [LoadmoreDatabase]
+})
+export class TaskTreeComponent implements OnInit {
+
+  nodeMap = new Map<string, LoadmoreFlatNode>();
+  treeControl: FlatTreeControl<LoadmoreFlatNode>;
+  treeFlattener: MatTreeFlattener<LoadmoreNode, LoadmoreFlatNode>;
+  // Flat tree data source
+  dataSource: MatTreeFlatDataSource<LoadmoreNode, LoadmoreFlatNode>;
+
+  constructor(
+    private taskDatabase: LoadmoreDatabase,
+    private masterService: MasterService,
+  ) {
+    this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel,
+      this.isExpandable, this.getChildren);
+
+    this.treeControl = new FlatTreeControl<LoadmoreFlatNode>(this.getLevel, this.isExpandable);
+
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    taskDatabase.dataChange.subscribe(data => {
+      this.dataSource.data = data;
+    });
+
+    taskDatabase.initialize();
+  }
+  ngOnInit(): void {
+
+  }
+
+  getChildren = (node: LoadmoreNode): Observable<LoadmoreNode[]> => node.childrenChange;
+
+  transformer = (node: LoadmoreNode, level: number) => {
+    const existingNode = this.nodeMap.get(node.item);
+
+    if (existingNode) {
+      return existingNode;
+    }
+
+    const newNode =
+      new LoadmoreFlatNode(node.item, level, node.hasChildren, node.loadMoreParentItem);
+    this.nodeMap.set(node.item, newNode);
+    return newNode;
+  }
+
+  getLevel = (node: LoadmoreFlatNode) => node.level;
+
+  isExpandable = (node: LoadmoreFlatNode) => node.expandable;
+
+  hasChild = (_: number, nodeData: LoadmoreFlatNode) => nodeData.expandable;
+
+  isLoadMore = (_: number, nodeData: LoadmoreFlatNode) => nodeData.item === LOAD_MORE;
+
+  /** Load more nodes from data source */
+  loadMore(item: string) {
+    this.taskDatabase.loadMore(item);
+  }
+
+  loadChildren(node: LoadmoreFlatNode) {
+    this.taskDatabase.loadMore(node.item, true);
+  }
+
+}
