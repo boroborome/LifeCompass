a7a17fba4a499f0a0c164530f8a3174a6b1c0a87
Yushan Gao <ysgao@thoughtworks.com>
2020-09-08 20:05:28 +0800

    chore: improve 404 logic, ignore normal file and request to backend servlet

--- backend/src/main/java/com/happy3w/lifecompass/component/Error404PageResolver.java
+++ backend/src/main/java/com/happy3w/lifecompass/component/Error404PageResolver.java
@@ -1,5 +1,6 @@
 package com.happy3w.lifecompass.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -11,13 +12,29 @@ import java.util.Map;
 @Component
 public class Error404PageResolver implements ErrorViewResolver {
 
+    @Value("${life-compass.service-path}/")
+    private String servicePath;
+
     @Override
     public ModelAndView resolveErrorView(HttpServletRequest request,
                                          HttpStatus status, Map<String, Object> model) {
-        if (status == HttpStatus.NOT_FOUND) {
+        String requestPath = (String) model.get("path");
+        if (status == HttpStatus.NOT_FOUND
+            && !requestPath.startsWith(servicePath)
+            && !isNormalResource(requestPath)) {
             return new ModelAndView("index.html");
         }
         return null;
+    }
+
+    private boolean isNormalResource(String contextPath) {
+        int pointIndex = contextPath.lastIndexOf('.');
+        if (pointIndex < 0) {
+            return false;
+        }
+
+        int slashIndex = contextPath.indexOf('/', pointIndex);
+        return slashIndex < 0;
     }
 
 }
