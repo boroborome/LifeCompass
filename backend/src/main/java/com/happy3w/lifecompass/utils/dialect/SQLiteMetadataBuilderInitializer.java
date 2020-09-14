d02a195d6e6ed0580b709137eac78f01a91768e2
Yushan Gao <ysgao@thoughtworks.com>
2020-09-14 22:04:57 +0800

    chore: support sqlite

---
+++ backend/src/main/java/com/happy3w/lifecompass/utils/dialect/SQLiteMetadataBuilderInitializer.java
@@ -0,0 +1,35 @@
+package com.happy3w.lifecompass.utils.dialect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.spi.MetadataBuilderInitializer;
+import org.hibernate.engine.jdbc.dialect.internal.DialectResolverSet;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolver;
+
+@Slf4j
+public class SQLiteMetadataBuilderInitializer implements MetadataBuilderInitializer {
+
+    @Override
+    public void contribute(MetadataBuilder metadataBuilder, StandardServiceRegistry serviceRegistry) {
+        DialectResolver dialectResolver = serviceRegistry.getService(DialectResolver.class);
+
+        if (!(dialectResolver instanceof DialectResolverSet)) {
+            log.warn("DialectResolver '%s' is not an instance of DialectResolverSet, not registering SQLiteDialect",
+                    dialectResolver);
+            return;
+        }
+
+        ((DialectResolverSet) dialectResolver).addResolver(resolver);
+    }
+
+    static private final SqliteDialect dialect = new SqliteDialect();
+
+    static private final DialectResolver resolver = (DialectResolver) info -> {
+        if (info.getDatabaseName().equals("SQLite")) {
+            return dialect;
+        }
+
+        return null;
+    };
+}
