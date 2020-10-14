2c1b7cfb633a460888e66b833284cd52abfd7a47
Yushan Gao <ysgao@thoughtworks.com>
2020-10-14 23:51:54 +0800

    chore: implement history-view with ng-zorro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -31,6 +31,9 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 registerLocaleData(zh);
 
@@ -61,6 +64,9 @@ registerLocaleData(zh);
     MatRadioModule,
     MatTooltipModule,
     BrowserAnimationsModule,
+    NzDatePickerModule,
+    NzFormModule,
+    NzTableModule,
 
     ServicesModule,
     FormsModule,
--- frontend/src/app/views/history-view/history-view.component.ts
+++ frontend/src/app/views/history-view/history-view.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
   selector: 'app-history-view',
@@ -6,10 +14,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-view.component.scss']
 })
 export class HistoryViewComponent implements OnInit {
+  validateForm!: FormGroup;
+  fieldStartTime = null;
 
-  constructor() { }
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({});
+    this.validateForm.addControl(`fieldStartTime`, new FormControl());
+    this.validateForm.addControl(`fieldEndTime`, new FormControl());
   }
 
+  resetForm(): void {
+
+  }
+
+  search(): void {
+
+  }
 }
